4f2c282e0febbc448fff62f7ee7e95581f7e0903
jiemooschina <460706836@qq.com>
2018-12-17 11:20:09 +0800

    update something

--- Automatic_detection_and_Inpainting_of_specular_reflections_Othmane_2011/main.m
+++ Automatic_detection_and_Inpainting_of_specular_reflections_Othmane_2011/main.m
@@ -6,12 +6,12 @@ dbstop if error
 addpath ./lib
 
 %% Detection
-img_path = 'imgs/2.bmp';
+img_path = 'imgs/fig10_b.bmp';
 %img_path = 'imgs/image-0001615.png';
 img = imread(img_path);
-specular_mask = SpecularDetectionOthmane2011(img);
+specular_mask = SpecularDetectionMeslouhi2011(img);
 %dilated_mask = imdilate(specular_mask, strel("diamond", 1));
-figure;imshow([specular_mask]);
+imshowpair(img, specular_mask, 'montage');
 
 %% Inpaiting
-result = inpainting(img, specular_mask, 0.05);
+%result = inpainting(img, specular_mask, 0.05);
